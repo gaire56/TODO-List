5904d439fb3c625d7e28b9975dcf48d8857bd893
Anant Gaire <56.anant@gmail.com>
2021-01-28 15:56:15 +0545

    input and add button added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className="app">
-      <h1>hi from app</h1>
-    </div>
+    <>
+      <div className="app">
+        <div className="center__div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder="Add a Item" />
+          <button> + </button>
+        </div>
+      </div>
+    </>
   );
 };
 
